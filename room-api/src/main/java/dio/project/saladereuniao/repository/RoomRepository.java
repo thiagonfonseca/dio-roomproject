1ad5c38fa60776ab637f01a1a168bfd2959c85ab
Thiago Fonseca <thiagonfonseca@gmail.com>
2021-01-11 22:54:52 -0300

    first commit

---
+++ room-api/src/main/java/dio/project/saladereuniao/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package dio.project.saladereuniao.repository;
+
+import dio.project.saladereuniao.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room,Long> {
+}
---
+++ room-api/src/test/java/dio/project/saladereuniao/SaladereuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.project.saladereuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaladereuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
